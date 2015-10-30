ca4417128040a464fd7e5a940596e05071f94d02
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-29 20:29:47 -0700

    Added index to username.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151030001343) do
+ActiveRecord::Schema.define(version: 20151030032646) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["username"], name: "index_users_on_username"
 
 end
