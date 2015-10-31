5bae0f7e39a316ba9d2a8765b0aa41e1b06275bf
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-10-30 22:58:42 -0700

    Add validation on Contest attributes.

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,2 +1,14 @@
 class Contest < ActiveRecord::Base
+  /\A[\w\s-]+\z/
+
+  validates :name, uniqueness: true, presence: true, length: {in: 1..50}
+  validates :url, uniqueness: false, presence: false, url: {:no_local => true}
+
+  before_save :downcase_name
+
+  private
+  def downcase_name
+    name.downcase!
+  end
+
 end
--- test/models/contest_test.rb
+++ test/models/contest_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class ContestTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @contest = Contest.new(name: 'TopCoder', url: 'http://www.topcoder.com',
+                           description: 'A vendor-neutral coding competition site')
+  end
+
+  test 'an example contest should be valid' do
+    assert @contest.valid?
+  end
+
+  test 'Name cannot be nil or empty' do
+    @contest.name = nil
+    assert_not @contest.valid?
+
+    @contest.name = ''
+    assert_not @contest.valid?
+
+    @contest.name = '        '
+    assert_not @contest.valid?
+  end
+
+  test 'description can be skipped' do
+    @contest.description = nil
+    assert @contest.valid?
+  end
+
+  test 'valid urls should be accepted' do
+    valid_urls = ['http://www.solve.org', 'https://www.chllaenge.io/tc', 'https://uhunt.felix-halim.net']
+    valid_urls.each do |url|
+      @contest.url = url
+      assert @contest.valid?, "#{url} should be valid"
+    end
+  end
+
+  test 'invalid urls should be rejected' do
+    invalid_urls = ['yolo.com, http://internal', 'localhost:8080']
+    invalid_urls.each do |url|
+      @contest.url = url
+      assert_not @contest.valid?, "#{url} should be invalid"
+    end
+  end
+
 end
