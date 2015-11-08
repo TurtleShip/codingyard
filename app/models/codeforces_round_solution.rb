385719b6c6aab05ff9a041ca3aa144f477c31f2f
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 12:01:39 -0800

    Added belongs_to test from CodeforcesRoundSolution to User and Contest.

--- app/models/codeforces_round_solution.rb
+++ app/models/codeforces_round_solution.rb
@@ -1,4 +1,7 @@
 class CodeforcesRoundSolution < ActiveRecord::Base
   belongs_to :user
   belongs_to :contest
+
+  validates :user_id, presence: true
+  validates :contest_id, presence: true
 end
--- test/models/codeforces_round_solution_test.rb
+++ test/models/codeforces_round_solution_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CodeforcesRoundSolutionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:Seulgi)
+    @codefores = contests(:Codeforces)
+    @solution = @user.codeforces_round_solutions.create(contest_id: @codefores.id,
+                                                        round_number: 1, division_number: 1, level: 'A')
+  end
+
+  test 'am example solution should be valid' do
+    assert @solution.valid?
+  end
+
+  test 'a solution should have a user id' do
+    @solution.user_id = nil
+    assert_not @solution.valid?
+  end
+
+  test 'a solution should have a contest id' do
+    @solution.contest_id = nil
+    assert_not @solution.valid?
+  end
+
 end
