15bb6c82d0e7f78d3649aa343ac50ee6ecfcb01e
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 15:36:31 -0800

    Genereate UserController and its skeleton codes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
