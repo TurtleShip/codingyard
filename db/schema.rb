42289e12cc3442ddc94a51ce0ca711d007bb4b7a
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 10:59:59 -0800

    Add difficulty field to TopCoderSRM.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108181044) do
+ActiveRecord::Schema.define(version: 20151108185820) do
 
   create_table "contests", force: :cascade do |t|
     t.string   "name",        null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20151108181044) do
     t.string   "save_path"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "difficulty",      null: false
   end
 
   add_index "top_coder_srm_solutions", ["contest_id"], name: "index_top_coder_srm_solutions_on_contest_id"
