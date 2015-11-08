3808426251e32af4c02340bccf0bc194a03e6c67
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 12:12:54 -0800

    Add validation for codeforces solutions.

--- app/models/codeforces_round_solution.rb
+++ app/models/codeforces_round_solution.rb
@@ -4,4 +4,14 @@ class CodeforcesRoundSolution < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :contest_id, presence: true
+  validates :round_number, presence: true, numericality: {greater_than: 0}
+  validates :division_number, presence: true, inclusion: {in: 1..2}
+  validates :level, presence: true, inclusion: %w(A B C D E)
+
+  before_validation :upcase_level
+
+  private
+    def upcase_level
+      level.upcase! unless level.nil?
+    end
 end
--- test/models/codeforces_round_solution_test.rb
+++ test/models/codeforces_round_solution_test.rb
@@ -23,4 +23,56 @@ class CodeforcesRoundSolutionTest < ActiveSupport::TestCase
     assert_not @solution.valid?
   end
 
+  test 'a solution should be a positive round number' do
+    @solution.round_number = nil
+    assert_not @solution.valid?
+
+    @solution.round_number = -1
+    assert_not @solution.valid?
+
+    @solution.round_number = 0
+    assert_not @solution.valid?
+
+    @solution.round_number = 1
+    assert @solution.valid?
+  end
+
+  test 'a solution should have a division number of either 1 or 2' do
+    invalid_division_numbers = [nil, -1, 0, 3, 5, 9999]
+    invalid_division_numbers.each do |division_number|
+      @solution.division_number = division_number
+      assert_not @solution.valid?
+    end
+
+    valid_division_numbers = [1, 2]
+    valid_division_numbers.each do |division_number|
+      @solution.division_number = division_number
+      assert @solution.valid?
+    end
+  end
+
+
+  test 'a solution should have a level of A,B,C,D or E' do
+    invalid_levels = [nil, '', 'hard', 'easy', '!!@#%']
+    invalid_levels.each do |level|
+      @solution.level = level
+      assert_not @solution.valid?, "#{level} should not be a valid level."
+    end
+
+    valid_levels = %w(A B C D E)
+    valid_levels.each do |level|
+      @solution.level = level
+      assert @solution.valid? "#{level} shbould be a valid level."
+    end
+  end
+
+  test 'level should be upcased before validation' do
+    valid_levels = %w(a b c d e)
+    valid_levels.each do |level|
+      @solution.level = level
+      @solution.save
+      assert_equal level.upcase, @solution.level
+    end
+  end
+
 end
