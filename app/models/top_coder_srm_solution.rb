ed7921bab14c3a0dc4afc51f07323aa239ec3be1
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 11:13:30 -0800

    Added validations on TopCoderSRMSolution.

--- app/models/top_coder_srm_solution.rb
+++ app/models/top_coder_srm_solution.rb
@@ -4,5 +4,8 @@ class TopCoderSrmSolution < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :contest_id, presence: true
+  validates :srm_number, presence: true, numericality: {greater_than: 0}
+  validates :division_number, presence: true, inclusion: 1..2
+  validates :difficulty, presence: true, inclusion: %w(easy medium hard)
 
 end
--- test/models/top_coder_srm_solution_test.rb
+++ test/models/top_coder_srm_solution_test.rb
@@ -6,7 +6,9 @@ class TopCoderSrmSolutionTest < ActiveSupport::TestCase
     @user = users(:Seulgi)
     @contest = contests(:TopCoder)
     @solution = @user.top_coder_srm_solutions.create(contest_id: @contest.id,
-                                                     srm_number: 256, division_number: 1, save_path: '/topcoder/256/div1/mid.cpp')
+                                                     srm_number: 256, division_number: 1,
+                                                     save_path: '/topcoder/256/div1/mid.cpp',
+                                                     difficulty: 'hard')
 
   end
 
@@ -25,6 +27,54 @@ class TopCoderSrmSolutionTest < ActiveSupport::TestCase
   test 'srm solution must have a contest id' do
     @solution.contest_id = nil
     assert_not @solution.valid?
+  end
+
+  test 'srm solution should have positive srm number' do
+    @solution.srm_number = nil
+    assert_not @solution.valid?
+
+    @solution.srm_number = -1
+    assert_not @solution.valid?
+
+    @solution.srm_number = 100
+    assert @solution.valid?
+  end
+
+  test 'srm solution should division number either 1 or 2' do
+    @solution.division_number = nil
+    assert_not @solution.valid?
+
+    @solution.division_number = -1
+    assert_not @solution.valid?
+
+    @solution.division_number = 0
+    assert_not @solution.valid?
+
+    @solution.division_number = 3
+    assert_not @solution.valid?
+
+    @solution.division_number = 1
+    assert @solution.valid?
+
+    @solution.division_number = 2
+    assert @solution.valid?
+  end
+
+  test 'srm solution should have difficulty in [easy, medium, hard]' do
+    @solution.difficulty = nil
+    assert_not @solution.valid?
+
+    invalid_difficulties = ['Kinda hard', '']
+    invalid_difficulties.each do |difficulty|
+      @solution.difficulty = difficulty
+      assert_not @solution.valid?, "#{difficulty} should not be valid"
+    end
+
+    valid_difficulties = %w(easy medium hard)
+    valid_difficulties.each do |difficulty|
+      @solution.difficulty = difficulty
+      assert @solution.valid?, "#{difficulty} should be valid"
+    end
   end
 
 
