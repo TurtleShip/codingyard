cc26e55f301685a9ecfe7ef3798b599a98264267
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-08 10:56:25 -0800

    Add a test for solution belongs to User and Contest.

--- app/models/top_coder_srm_solution.rb
+++ app/models/top_coder_srm_solution.rb
@@ -1,4 +1,8 @@
 class TopCoderSrmSolution < ActiveRecord::Base
   belongs_to :user
   belongs_to :contest
+
+  validates :user_id, presence: true
+  validates :contest_id, presence: true
+
 end
--- test/models/top_coder_srm_solution_test.rb
+++ test/models/top_coder_srm_solution_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class TopCoderSrmSolutionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:Seulgi)
+    @contest = contests(:TopCoder)
+    @solution = @user.top_coder_srm_solutions.create(contest_id: @contest.id,
+                                                     srm_number: 256, division_number: 1, save_path: '/topcoder/256/div1/mid.cpp')
+
+  end
+
+  test 'an example solution must be valid' do
+    assert @solution.valid?
+    assert @user.save
+    assert @contest.save
+    assert @solution.save
+  end
+
+  test 'srm solution must have a user id' do
+    @solution.user_id = nil
+    assert_not @solution.valid?
+  end
+
+  test 'srm solution must have a contest id' do
+    @solution.contest_id = nil
+    assert_not @solution.valid?
+  end
+
+
 end
