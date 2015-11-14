79df7ec170e760fdcec815f8b9826f6b8753f502
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-13 21:11:42 -0800

    Add user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'Welcome to CodingYard!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user)
+          .permit(:username, :email, :firstname, :lastname,
+                  :password, :password_confirmation)
+    end
+
 end
