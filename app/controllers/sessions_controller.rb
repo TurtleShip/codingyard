5e208705f477a0f72bcf6f5d056d9e88df8ff26c
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-15 00:32:13 -0600

    Added log_in, current_user, and logged_in? method to SessionsHelper module.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,10 @@ class SessionsController < ApplicationController
   def create
     params = login_params
     user = User.find_by_username(params[:username])
-    if user && user.authenticate(params[:password])
-      # Log the user in and redirect to the user's show page.
 
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
