7586e223b66b5abe684bc92a2118cb9d6346becf
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-14 22:35:54 -0600

    Provide singup link in root page.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,6 +7,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path
     assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', signup_path
   end
 
 end
