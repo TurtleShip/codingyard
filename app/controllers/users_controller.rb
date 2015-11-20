a78434a491c575e9aa474c184494d991bde6bf08
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-20 15:51:03 -0800

    Handle successful and unsuccessful user profile update.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Your profile has been successfully updated.'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
     def user_params
       params.require(:user)
