e591d586c58382f96f49223f041bfcaf9b47d5ea
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-20 23:46:14 -0800

    Add tests for visiting profile edit page when not logged in.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:Seulgi)
+  end
+
+  test 'should redirect edit when not logged in' do
+    get :edit, id: @user
+    assert_not_empty flash
+    assert_redirected_to login_url
+  end
+
+  test 'should redirect update when not logged in' do
+    patch :update, id: @user, user: {
+                     username: 'valid_new_name'
+                 }
+    assert_not_empty flash
+    assert_redirected_to login_url
+  end
+
 end
