9dcd0db3db376130e71b979e74aa1e91e03fe91a
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-22 19:30:35 -0800

    Add test coverage for site layout.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,12 +2,27 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test 'layout link when a user is not logged in' do
+  def setup
+    @member = users(:Taejung)
+  end
+
+  test 'layout links when a user is not logged in' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path
     assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', signup_path
+  end
+
+  test 'layout links when a user is logged in' do
+    log_in_as @member
+    get root_path
+    assert_select 'a[href=?]', root_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', user_path(@member)
+    assert_select 'a[href=?]', edit_user_path(@member)
+    assert_select 'a[href=?]', logout_path
   end
 
 end
