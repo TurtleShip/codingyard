593312d679bb2493d249b202f272fb6d434d915b
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-22 20:09:22 -0800

    Create activation token/digest before creating a user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   VALID_NAME_REGEX = /\A['\w\s-]*\z/
 
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   has_many :top_coder_srm_solutions, dependent: :destroy
   has_many :codeforces_round_solutions, dependent: :destroy
@@ -23,6 +23,7 @@ class User < ActiveRecord::Base
   validates :lastname, presence: false, uniqueness: false, format: {with: VALID_NAME_REGEX}
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
+  before_create :create_activation_digest
   before_save :downcase_email
   before_save :downcase_username
 
@@ -65,4 +66,9 @@ class User < ActiveRecord::Base
       email.downcase!
     end
 
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
