9cf72e395e29c5f1f91daad7a982d95fb4299b0e
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-27 12:24:19 -0800

    Support account activation through email.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = 'Welcome to CodingYard!'
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = 'Please check your email to activate your account. It may be in your junk folder.'
+      redirect_to root_url
     else
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -47,9 +47,12 @@ class User < ActiveRecord::Base
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password? remember_token
+  def authenticated?(attribute, token)
+    # This is metaprogramming ( a programming writing a program)
+    # It is equivalent to calling self.attribute_digest
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
