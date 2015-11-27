390014527926a6782eb432d635be2c37a4309da3
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-27 12:47:25 -0800

    Refactor user activation account code to user class.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account. It may be in your junk folder.'
       redirect_to root_url
     else
