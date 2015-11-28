970ca25e397494cc39047905e63c0068cdf7922d
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-27 16:28:21 -0800

    Handle password rest form submission.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,9 @@
 class PasswordResetsController < ApplicationController
 
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -18,4 +22,44 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, 'Can\'t be empty')
+      render 'edit'
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = 'Password has been reset.'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    # Before filters
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+        redirect_to root_url
+      end
+    end
+
+    # Check expiration of reset token.
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = 'Password reset has expired.'
+        redirect_to new_password_reset_url
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,12 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
 
+  # Returns true if a password reset has expired.
+  def password_reset_expired?
+    # Is password reset sent EARLIER than two ours ago?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
     def downcase_username
       username.downcase!
