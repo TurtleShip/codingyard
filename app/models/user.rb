0070318404eb0bd54256cd9e36529151bfc3b9b0
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-27 21:09:31 -0800

    Remove unncessary assignment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -73,7 +73,7 @@ class User < ActiveRecord::Base
   # Creates reset digest
   def create_reset_digest
     self.reset_token = User.new_token
-    update_columns(reset_digest: self.reset_digest = User.digest(reset_token),
+    update_columns(reset_digest: User.digest(reset_token),
                    reset_sent_at: Time.zone.now )
   end
 
