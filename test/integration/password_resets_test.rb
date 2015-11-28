df17c9b3cf3702650eb7741e9e493bbbb7971a68
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-11-27 20:55:27 -0800

    Add integration test for expired token.

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -76,4 +76,21 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     assert_redirected_to user, 'successful password reset should redirect user to its profile page'
   end
 
+  test 'expired token' do
+    get new_password_reset_path
+    post password_resets_path, password_reset: {email: @user.email}
+
+    @user = assigns(:user)
+    @user.update_attribute(:reset_sent_at, 3.hours.ago)
+    patch password_reset_path(@user.reset_token),
+          email: @user.email,
+          user: {
+              password: 'new_password_!',
+              password_confirmation: 'new_password_!'
+          }
+    assert_response :redirect
+    follow_redirect!
+    assert_match /expired/i, response.body
+  end
+
 end
