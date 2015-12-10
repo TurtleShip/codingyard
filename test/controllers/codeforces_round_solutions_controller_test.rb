6ea33006b67bfac6ae827df6a16df3d8ec475d6a
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-10 15:29:01 -0800

    Add tests for CodeforcesRoundSolutionController#index.

--- test/controllers/codeforces_round_solutions_controller_test.rb
+++ test/controllers/codeforces_round_solutions_controller_test.rb
@@ -8,8 +8,11 @@ class CodeforcesRoundSolutionsControllerTest < ActionController::TestCase
     @other_member = users(:Hansol)
     language = languages(:Java)
     params = {round_number: 1, division_number: 1, level: 'A'}
+    params_other = { round_number: 3, division_number: 2, level: 'B'}
     @solution = CodeforcesRoundSolution.new_with_relations(params, @member, language)
     @solution.save
+    @other_solution = CodeforcesRoundSolution.new_with_relations(params_other, @other_member, language)
+    @other_solution.save
   end
 
   test 'a user must login to upload a solution' do
@@ -43,6 +46,37 @@ class CodeforcesRoundSolutionsControllerTest < ActionController::TestCase
     delete :destroy, id: @solution.id
     assert_redirected_to codeforces_round_solutions_url
     assert_not_empty flash
+  end
+
+  test 'anyone can view a solution' do
+    get :show, id: @solution.id
+    assert_response :success
+
+    assert_select 'div.solution_display_value', @solution.round_number.to_s
+    assert_select 'div.solution_display_value', @solution.division_number.to_s
+    assert_select 'div.solution_display_value', @solution.level
+    assert_select 'div.solution_display_value', @solution.language.name
+  end
+
+  test 'anyone can view all solutions' do
+    get :index
+    assert_response :success
+
+    assert_select 'td', @member.username
+    assert_select 'td', @solution.language.name
+    assert_select 'td', @solution.round_number.to_s
+    assert_select 'td', @solution.division_number.to_s
+    assert_select 'td', @solution.level
+    assert_select 'a[href=?]', codeforces_round_solution_path(@solution)
+    assert_select 'a[data-method="delete"]', :href => codeforces_round_solution_path(@solution)
+
+    assert_select 'td', @other_member.username
+    assert_select 'td', @other_solution.language.name
+    assert_select 'td', @other_solution.round_number.to_s
+    assert_select 'td', @other_solution.division_number.to_s
+    assert_select 'td', @other_solution.level
+    assert_select 'a[href=?]', codeforces_round_solution_path(@other_solution)
+    assert_select 'a[data-method="delete"]', :href => codeforces_round_solution_path(@other_solution)
   end
 
 end
