4a77ddba8388fafb3a4e8b79ab0ab03d07b81d00
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-23 23:27:53 -0800

    Formatted user index nicer.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,7 +75,8 @@ class UsersController < ApplicationController
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to root_url unless current_user?(@user)
+
+    redirect_to (request.referer || root_url) unless current_user?(@user)
   end
 
   def admin_user
