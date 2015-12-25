a0c0c8c5e52b130fa347984637f3acd695a6c34b
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-25 01:34:37 -0800

    Create skeleton files for TopCoderSrmSolutionsController.

---
+++ app/controllers/top_coder_srm_solutions_controller.rb
@@ -0,0 +1,25 @@
+class TopCoderSrmSolutionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def download
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
