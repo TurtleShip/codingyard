46415d6a419c716c47f0c68298f386c7ce05d8b8
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-25 16:39:43 -0800

    Have TopCoderSrmSolutionsController to extend SolutionsController

--- app/controllers/top_coder_srm_solutions_controller.rb
+++ app/controllers/top_coder_srm_solutions_controller.rb
@@ -1,3 +1,31 @@
-class TopCoderSrmSolutionsController < ApplicationController
+class TopCoderSrmSolutionsController < SolutionsController
+
+  private
+  def solution_class
+    TopCoderSrmSolution
+  end
+
+  def solution_params
+    params.required(:top_coder_srm_solution)
+        .permit(:srm_number, :division_number, :difficulty, :original_link)
+  end
+
+  def search_params
+    params.permit(:srm_number, :division_number, :difficulty, :author, :language)
+  end
+
+  def attachment_param
+    params[:top_coder_srm_solution][:attachment]
+  end
+
+  def solution_info
+    {
+        SRM: @solution.srm_number,
+        Division: @solution.division_number,
+        difficulty: @solution.difficulty,
+        Language: @solution.language.name,
+        'Original problem': @solution.original_link
+    }
+  end
 
 end
