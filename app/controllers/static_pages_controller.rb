1ee36cc9fb7176f51f7b4b33478cab563d2c42eb
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-27 22:57:11 -0800

    Show the number of solutions being hosted on the index page.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,8 @@
 class StaticPagesController < ApplicationController
+
+  def home
+    @codeforces_total = CodeforcesRoundSolution.count
+    @topcoder_srm_total = TopCoderSrmSolution.count
+    @uva_total = UvaSolution.count
+  end
 end
