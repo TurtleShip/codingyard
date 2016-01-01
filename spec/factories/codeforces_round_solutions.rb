397bafe9196ef4daaffd279479f6b3ec1bd40de1
Seulgi Kim <seulgi.kim.56@outlook.com>
2015-12-31 16:55:02 -0800

    Factor out codeforces test.

--- spec/factories/codeforces_round_solutions.rb
+++ spec/factories/codeforces_round_solutions.rb
@@ -1,15 +1,16 @@
 FactoryGirl.define do
 
+  levels = %w(A B C D E)
+
   factory :codeforces_round_solution do
     user
     language
-    round_number Faker::Number.number(3)
-    division_number Faker::Number.between(1, 2)
-    level %w(A B C D E)[Faker::Number.between(0, 4)]
+    sequence(:round_number) { |n| n }
+    sequence(:division_number) { |n| (n%2) + 1 }
+    sequence(:level) { |idx| levels[idx % 5] }
 
-    before(:create) do |solution|
-      @codeforces ||= FactoryGirl.create(:topcoder)
-      solution.contest = @codeforces
+    callback(:after_build, :before_create) do |solution|
+      solution.contest = find_or_create(:topcoder)
     end
   end
 
--- spec/models/codeforces_round_solution_spec.rb
+++ spec/models/codeforces_round_solution_spec.rb
@@ -1,4 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe CodeforcesRoundSolution, type: :model do
+
+  before(:each) do
+    user = create(:user)
+    cpp = create(:cpp)
+    @solution = build(:codeforces_round_solution, user: user, language: cpp)
+  end
+
+  it 'has a valid factory' do
+    expect(@solution).to be_valid
+  end
+
+  it { should belong_to(:user) }
+  it { should belong_to(:contest) }
+  it { should belong_to(:language) }
+
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:contest_id) }
+  it { should validate_presence_of(:language_id) }
+
+  it { should validate_presence_of(:round_number) }
+  it { should validate_numericality_of(:round_number).is_greater_than(0) }
+
+  it { should validate_presence_of(:division_number) }
+  it { should validate_inclusion_of(:division_number).in_range(1..2) }
+
+  it { should validate_presence_of(:level) }
+  it { should validate_inclusion_of(:level).in_array(%w(A B C D E)) }
+
+  it { should_not validate_presence_of(:original_link) }
+  it { should validate_length_of(:original_link).is_at_most(255) }
+
 end
